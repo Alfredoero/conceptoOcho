e01de4ceeb721f8da39707a6874a3be449906934
Alfredoero <enriquea.rodriguezr@gmail.com>
2017-09-06 17:00:07 -0400

    update commit bootstrap

--- main/forms.py
+++ main/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 class PostForm(forms.Form):
 	do_search = forms.CharField(label='Search', max_length=200)
-	num_results = forms.CharField(label='Result Number', max_length=20)
+	#num_results = forms.CharField(label='Result Number', max_length=20)
 	search_city = forms.CharField(label='City', max_length=200)
+	search_country = forms.CharField(label='Country', max_length=150)
--- main/views.py
+++ main/views.py
@@ -11,20 +11,28 @@ import pprint
 import json
 
 # Create your views here.
+def get_keywords(meta):
+	print meta["content"]
+	return
 def run_script(url):
 
 	return
 	
 def index(request):
+	page = 0
 	if request.method == 'POST':
 		form = PostForm(request.POST)
 		if form.is_valid():
 			data = form.cleaned_data['do_search']
+			#num_results = form.cleaned_data['num_results']
+			search_city = form.cleaned_data['search_city']
+			search_country = form.cleaned_data['search_country'] 
 			page = request.POST.get("page")
-			print page
 			service = build("customsearch", "v1", developerKey="AIzaSyBfsEcEcNt4wtZq7iM5LV2gWfwnSQAD0cA")
-		  	res = service.cse().list( q=data, cx='011980423541542895616:ug0kbjbf6vm', gl='us', ).execute()
+		  	res = service.cse().list( q=data, cx='011980423541542895616:ug0kbjbf6vm', gl=search_country, hq="near=%s" % search_city, cr=search_country, hl=search_country, filter="1", ).execute()
 		  	total = res["searchInformation"]["totalResults"]
+		  	#pprint.pprint(res)
+		  	print total
 		  	all_links = []
 		  	n_total = []
 		  	all_metas = []
@@ -43,16 +51,15 @@ def index(request):
 			  		html_doc = urllib2.urlopen(link)
 			  		soup = BeautifulSoup(html_doc, 'html.parser')			  		
 			  		for met in soup.findAll(attrs={"name":"keywords"}):
-			  			print met
+			  			get_keywords(met)
 			  			metas.append(met.encode("utf-8"))
 			  		all_metas.append({"link": link , "meta": metas})
 			  	except urllib2.HTTPError as error:
 			  		all_metas.append({"link": link , "meta": "Forbidden %s" % error.code})
-			  		print "Forbidden %s" %(error.code)
-			pprint.pprint(all_metas)
+			  		#print "Forbidden %s" %(error.code)
+			#pprint.pprint(all_metas)
 			return render(request, 'main/index.html', {'page': page, 'data': all_metas, 'form': form})
 	else:
-		page = 0
 		form = PostForm()
 	return render(request, 'main/index.html', {'page': page, 'form': form})
 	
