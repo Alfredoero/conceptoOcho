a32e2d6d0d646b3ee3c6a077c5e446983b7e3d96
Alfredoero <enriquea.rodriguezr@gmail.com>
2017-09-07 15:09:41 -0400

    update 4

--- conceptoOcho/urls.py
+++ conceptoOcho/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from .main import views
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),    
-    url(r'^check1/$', views.index, name='index')
+    url(r'^check1/$', views.check1, name='index')
 ]
