fffc867889e2eec8285b43661f79cd1fbd4f97b7
Alfredoero <enriquea.rodriguezr@gmail.com>
2017-09-07 13:31:45 -0400

    to heroku

--- main/views.py
+++ main/views.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
-import urllib2
+import urllib.request as urllib
 from .forms import PostForm
 from bs4 import BeautifulSoup
 from googleapiclient.discovery import build
@@ -12,7 +12,7 @@ import json
 
 # Create your views here.
 def get_keywords(meta):
-	print meta["content"]
+	print(meta["content"])
 	return
 def run_script(url):
 
@@ -29,34 +29,34 @@ def index(request):
 			search_country = form.cleaned_data['search_country'] 
 			page = request.POST.get("page")
 			service = build("customsearch", "v1", developerKey="AIzaSyBfsEcEcNt4wtZq7iM5LV2gWfwnSQAD0cA")
-		  	res = service.cse().list( q=data, cx='011980423541542895616:ug0kbjbf6vm', gl=search_country, hq="near=%s" % search_city, cr=search_country, hl=search_country, filter="1", ).execute()
-		  	total = res["searchInformation"]["totalResults"]
-		  	#pprint.pprint(res)
-		  	print total
-		  	all_links = []
-		  	n_total = []
-		  	all_metas = []
-		  	for item in res["items"]:
-		  		all_links.append(item["link"])		  		
+			res = service.cse().list( q=data, cx='011980423541542895616:ug0kbjbf6vm', gl=search_country, hq="near=%s" % search_city, cr=search_country, hl=search_country, filter="1", ).execute()
+			total = res["searchInformation"]["totalResults"]
+			#pprint.pprint(res)
+			print(total)
+			all_links = []
+			n_total = []
+			all_metas = []
+			for item in res["items"]:
+				all_links.append(item["link"])              
 	
-		  	#for x in xrange(1, 10):
-		  	#	n_total.append(x)
-		  	#	res2 = service.cse().list( q=data, cx='011980423541542895616:ug0kbjbf6vm', gl='us', start=(x*10)+1, ).execute()
-		  	#	for item in res2["items"]:
-		  	#		all_links.append(item["link"])
+			#for x in xrange(1, 10):
+			#   n_total.append(x)
+			#   res2 = service.cse().list( q=data, cx='011980423541542895616:ug0kbjbf6vm', gl='us', start=(x*10)+1, ).execute()
+			#   for item in res2["items"]:
+			#       all_links.append(item["link"])
 
-		  	for link in all_links:
-		  		metas = []
-		  		try:
-			  		html_doc = urllib2.urlopen(link)
-			  		soup = BeautifulSoup(html_doc, 'html.parser')			  		
-			  		for met in soup.findAll(attrs={"name":"keywords"}):
-			  			get_keywords(met)
-			  			metas.append(met.encode("utf-8"))
-			  		all_metas.append({"link": link , "meta": metas})
-			  	except urllib2.HTTPError as error:
-			  		all_metas.append({"link": link , "meta": "Forbidden %s" % error.code})
-			  		#print "Forbidden %s" %(error.code)
+			for link in all_links:
+				metas = []
+				try:
+					html_doc = urllib.urlopen(link)
+					soup = BeautifulSoup(html_doc, 'html.parser')                   
+					for met in soup.findAll(attrs={"name":"keywords"}):
+						get_keywords(met)
+						metas.append(met.encode("utf-8"))
+					all_metas.append({"link": link , "meta": metas})
+				except urllib2.HTTPError as error:
+					all_metas.append({"link": link , "meta": "Forbidden %s" % error.code})
+					#print "Forbidden %s" %(error.code)
 			#pprint.pprint(all_metas)
 			return render(request, 'main/index.html', {'page': page, 'data': all_metas, 'form': form})
 	else:
