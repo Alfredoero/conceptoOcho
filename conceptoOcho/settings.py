3d3cbd320411856d041e077393414d1559073707
Alfredoero <enriquea.rodriguezr@gmail.com>
2017-09-07 14:03:40 -0400

    allow host

--- conceptoOcho/settings.py
+++ conceptoOcho/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^*6+d0_8np5jeb5p!)6nu*@^o5g1zo0^zz7p6ec%rh@q-e#=8t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["secret-taiga-35569.herokuapp.com"]
 
 
 # Application definition
