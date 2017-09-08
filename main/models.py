c396efb9aa94e7dfba6e8376ee22f1a0c7ba3e40
Alfredoero <enriquea.rodriguezr@gmail.com>
2017-09-08 09:52:54 -0400

    update

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # Create your models here.
 
 class Search(models.Model):
-	site_name = models.CharField(unique=True, max_length=300)
+	site_name = models.CharField(max_length=300)
 	site_url = models.CharField(max_length=2000)
 	site_weight = models.CharField(max_length=10, blank=True, null=True)
 	site_status = models.CharField(max_length=200, blank=True, null=True)
