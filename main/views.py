c1aa5de493d675e0f26b81066c2613aec85e5b46
Alfredoero <enriquea.rodriguezr@gmail.com>
2017-09-08 08:26:41 -0400

    update

--- main/views.py
+++ main/views.py
@@ -106,7 +106,7 @@ def filter(request):
 		for page in Search.objects.all():		
 			html_doc = urllib.request.urlopen(page.site_url)
 			soup = BeautifulSoup(html_doc, 'html.parser')
-			info = soup.body.find(text=re.compile('^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$'))
+			info = soup.body.findAll(text=re.compile('^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$'))
 			contact.append({"url": page.site_url, "info": info })
 
 		return render(request, 'main/filter.html', {'contact': contact })
