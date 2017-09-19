546fe14458acd6d4b8f140927a389836cab70e38
Alfredoero <enriquea.rodriguezr@gmail.com>
2017-09-19 13:10:19 -0400

    update

--- main/views.py
+++ main/views.py
@@ -131,7 +131,7 @@ def get_info(url):
 		if contact != "no response":
 
 			for cont in list(set(contact)):
-				if valid_url(con):
+				if valid_url(cont):
 					html_doc = urllib.request.urlopen(cont)
 					soup = BeautifulSoup(html_doc, 'html.parser')
 					info = soup.findAll(text=re.compile('^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$'))
