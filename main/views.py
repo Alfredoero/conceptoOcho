00c7fdc49f7dc5fcebee41dc2e8efe90f7cf9581
Alfredoero <enriquea.rodriguezr@gmail.com>
2017-09-19 14:06:18 -0400

    update

--- main/views.py
+++ main/views.py
@@ -136,7 +136,7 @@ def get_info(url):
 					info = soup.findAll(text=re.compile('^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$'))
 					email = soup.findAll(text=re.compile('(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)'))
 					return {"url": contact["url"], "info": info, 'email': email}
-				else if cont == "":
+				elif cont == "":
 					return {"url": contact["url"], "info": "No Valid or empty", "email": "No Valid or empty URL"}
 				else:
 					return {"url": cont, "info": "No Valid URL", "email": "No Valid URL"}
