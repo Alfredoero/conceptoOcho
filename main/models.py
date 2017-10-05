695bebb3d34eef3504ec7f1d5dd51b1b15d3eae0
Alfredoero <enriquea.rodriguezr@gmail.com>
2017-10-05 15:49:52 -0400

    added models Phone, Keyword, InfoSearch

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import datetime
 
 # Create your models here.
 
@@ -12,3 +13,20 @@ class Search(models.Model):
 	site_status = models.CharField(max_length=200, blank=True, null=True)
 
 
+class Phone(models.Model):
+	phone = models.CharField(max_length=20)
+
+
+class Keyword(models.Model):
+	keyword = models.CharField(max_length=300)
+
+
+class InfoSearch(models.Model):
+	site_name = models.CharField(max_length=300, blank=True, null=True)
+	site_url = models.CharField(max_length=2000)
+	site_phones = models.ManyToManyField(Phone, blank=True)
+	site_keywords = models.ManyToManyField(Keyword, blank=True)
+	site_email = models.CharField(max_length=300, blank=True, null=True)
+	site_address = models.CharField(max_length=500, blank=True, null=True)
+	site_contact_url = models.CharField(max_length=2000, blank=True, null=True)
+	search_date = models.DateField(auto_now_add=True, blank=True)
